ce31db9dd82c5d127359e5b9df00f9db8ea03701
irmk323 <irmk323@yahoo.co.jp>
2022-02-15 15:39:08 +0000

    Initial Commit

---
+++ src/main/java/com/test/assignment0215/Assignment0215Application.java
@@ -0,0 +1,13 @@
+package com.test.assignment0215;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment0215Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment0215Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/assignment0215/Assignment0215ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.assignment0215;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment0215ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
